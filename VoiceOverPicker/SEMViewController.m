77229ba8ac8ed02c1761f5d4a1f3ae9aa3505de8
Steve Moser <smoser@skookum.com>
2014-01-19 16:22:04 -0500

    example default picker with no voiceover support.

--- VoiceOverPicker/SEMViewController.m
+++ VoiceOverPicker/SEMViewController.m
@@ -8,7 +8,10 @@
 
 #import "SEMViewController.h"
 
-@interface SEMViewController ()
+@interface SEMViewController () <UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIPickerView *normalPicker;
 
 @end
 
@@ -18,12 +21,45 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIPickerView *inputViewPicker = [[UIPickerView alloc] init];
+    [inputViewPicker setDataSource:self];
+    [inputViewPicker setDelegate:self];
+    
+    [self.textField setInputView:inputViewPicker];
+    
+    [self.normalPicker setIsAccessibilityElement:YES];
+    [self.normalPicker becomeFirstResponder];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 4;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (row == 0) {
+        return @"one fish";
+    } else if (row == 1) {
+        return @"two fish";
+    } else if (row == 2) {
+        return @"red fish";
+    } else if (row == 3) {
+        return @"blue fish";
+    }
+    
+    return @"";
 }
 
 @end
